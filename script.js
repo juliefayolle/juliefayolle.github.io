71a0cfae899563e90bbac7b22b9c7a4a4090c443
admin <admin@iMac3.local>
2017-11-03 16:53:30 +0100

    Version 1.5

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
   $('a[data-action="getResult"]').on('click', function(e) {
     e.preventDefault();
-    $.each($('#knowMe input'), function(index, value) {
-      if($(value).val() == 1) {
-          debugger;
-          $(value).css('background-color', 'green');
-      }
-    })
-  })
+    $('fieldset#diplome').prepend('<h4 class="style-reponses">BTS Communication</h4>');
+    $('fieldset#formation').prepend('<h4 class="style-reponses">Vrai</h4>');
+    $('fieldset#personnalite').prepend('<h4 class="style-reponses">Curieuse, patiente et créative</h4>');
+    $('fieldset#stage').prepend('<h4 class="style-reponses">Janvier 2018</h4>');
 
+  })
 });
